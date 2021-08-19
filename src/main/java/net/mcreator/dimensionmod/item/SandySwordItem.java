1f3f4704e898654947e4f814cfb1820692185761
amogusamogus <leon@LAPTOP-L4LMOLKA>
2021-08-19 14:15:11 +0100

    sandy armor

---
+++ src/main/java/net/mcreator/dimensionmod/item/SandySwordItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.dimensionmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.dimensionmod.DimensionModModElements;
+
+@DimensionModModElements.ModElement.Tag
+public class SandySwordItem extends DimensionModModElements.ModElement {
+	@ObjectHolder("dimension_mod:sandy_sword")
+	public static final Item block = null;
+	public SandySwordItem(DimensionModModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1800;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 7f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(SandyIngotItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("sandy_sword"));
+	}
+}
