961b59d9aba7c29f1334953948448aa2ce6ddf2e
flustered <cris@octophin.co.uk>
2021-08-21 14:00:58 +0100

    Frizen sand x,y,z melt

--- src/main/java/net/mcreator/dimensionmod/procedures/FrozenSandMeltProcedure.java
+++ src/main/java/net/mcreator/dimensionmod/procedures/FrozenSandMeltProcedure.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 
+import net.mcreator.dimensionmod.block.FrozenSandBlock;
 import net.mcreator.dimensionmod.DimensionModModElements;
 import net.mcreator.dimensionmod.DimensionModMod;
 
@@ -41,10 +42,60 @@ public class FrozenSandMeltProcedure extends DimensionModModElements.ModElement 
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		{
-			BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
-			BlockState _bs = Blocks.SAND.getDefaultState();
-			world.setBlockState(_bp, _bs, 3);
+		double SandMeltRedius = 0;
+		double Counter = 0;
+		SandMeltRedius = (double) 5;
+		Counter = (double) (SandMeltRedius);
+		for (int index0 = 0; index0 < (int) ((SandMeltRedius)); index0++) {
+			Counter = (double) ((Counter) - 1);
+			if (((world.getBlockState(new BlockPos((int) (x + (Counter)), (int) y, (int) z))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) (x + (Counter)), (int) y, (int) z);
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
+			if (((world.getBlockState(new BlockPos((int) (x - (Counter)), (int) y, (int) z))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) (x - (Counter)), (int) y, (int) z);
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
+			if (((world.getBlockState(new BlockPos((int) x, (int) (y + (Counter)), (int) z))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) (y + (Counter)), (int) z);
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
+			if (((world.getBlockState(new BlockPos((int) x, (int) (y - (Counter)), (int) z))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) (y - (Counter)), (int) z);
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
+			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) (z + (Counter))))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) (z + (Counter)));
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
+			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) (z - (Counter))))).getBlock() == FrozenSandBlock.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) (z - (Counter)));
+					BlockState _bs = Blocks.SAND.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+			}
 		}
 	}
 }
