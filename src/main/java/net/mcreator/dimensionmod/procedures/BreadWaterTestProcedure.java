940a1201d514f8fe27de70afc15715771c1425f8
flustered <cris@octophin.co.uk>
2021-10-10 14:58:11 +0100

    Lightning strike

--- src/main/java/net/mcreator/dimensionmod/procedures/BreadWaterTestProcedure.java
+++ src/main/java/net/mcreator/dimensionmod/procedures/BreadWaterTestProcedure.java
@@ -1,7 +1,17 @@
 package net.mcreator.dimensionmod.procedures;
 
-public class BreadWaterTestProcedure {
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.Explosion;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 
+import net.mcreator.dimensionmod.potion.NormalBreadEatenPotionEffect;
+import net.mcreator.dimensionmod.DimensionModMod;
+
+import java.util.Map;
+
+public class BreadWaterTestProcedure {
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
@@ -28,13 +38,11 @@ public class BreadWaterTestProcedure {
 				DimensionModMod.LOGGER.warn("Failed to load dependency world for procedure BreadWaterTest!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
 		if ((entity.isInWater())) {
 			if (world instanceof World && !((World) world).isRemote) {
 				((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 7, Explosion.Mode.BREAK);
@@ -44,5 +52,4 @@ public class BreadWaterTestProcedure {
 			}
 		}
 	}
-
 }
